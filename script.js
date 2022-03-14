32b4590a3a5b1c119db57f1d38187ec164d61eef
mercyakuku <mercyakuku24@gmail.com>
2022-03-14 22:53:25 +0300

    Add updated styles in jquery and css

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
-    });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
-    });
-  });
+//$(document).ready(function(){
+   // $("#development-image").click(function(){
+    //  $("#development-image").slideDown('1500').hide('1000');
+    //  $("#development").show('1500');
+  //  });
+    //$("#development").click(function(){
+      //$("#development").slideUp('1500');
+     // $("#development-image").slideDown('1500');
+    //});
+  //});
   
-  $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
+ $(document).ready(function(){
+    $("#image1").click(function(){
+      $(".design-text").show('1500');
+      $("#image1").hide();
     });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
-    });
+    $(".design-text").click(function(){
+      $("#image1").show('1500');
+      $(".design-text").hide();
   });
+});
   
-  $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
-    });
-  });
+  //$(document).ready(function(){
+  //  $("#product-image").click(function(){
+    //  $("#product-image").slideDown('1500').hide('1000');
+  //    $("#product").show('1500');
+    //});
+  //  $("#product").click(function(){
+     // $("#product").slideUp('1500');
+    //  $("#product-image").slideDown('1500');
+    //});
+ // });//
